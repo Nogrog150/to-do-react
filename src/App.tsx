71c04bbb7f5ca8c71a861b7d02e370899a296733
Nog <maurinho1402@hotmail.com>
2025-08-22 10:22:16 -0300

    Criando componente icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import Text from "./components/Text"
+import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import Icon from "./components/icon"
 
 function App() {
-
   return (
-    <>
-      <Text>Olá mundo</Text>
-    </>
-  )
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-1">
+        <Text variant={"body-sm-bold"} className="text-pink-base">
+          Olá mundo
+        </Text>
+        <Text className="text-green-base">
+          Olá mundo
+        </Text>
+        <Text variant={"body-md-bold"}>
+          Olá mundo
+        </Text>
+        <Text>Levar o dog para passear</Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-base"/>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
